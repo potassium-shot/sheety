b08838f05511a0d0f6f79dda3cfdc3d4279a7028
Toni Z <tonizana.dev@gmail.com>
2023-04-29 14:59:26 +0200

    Add doc comments for all of distribution.rs

--- sheety/src/distribution.rs
+++ sheety/src/distribution.rs
@@ -1,13 +1,25 @@
 use crate::utils::IVec2;
 
+/// Reprensents a constraint to follow when building [SpriteSheet][crate::SpriteSheet] from an
+/// [UnorderedSpriteSheet][crate::UnorderedSpriteSheet].
 #[derive(Debug, Clone)]
 pub enum Distribution {
+    /// Requires that the [SpriteSheet][crate::SpriteSheet] has a fixed amount of columns.
+    /// The number of lines will then depend on the amount of [Sprite][crate::Sprite]s in the sheet.
     FixedColumns(usize),
+    /// Requires that the [SpriteSheet][crate::SpriteSheet] has a fixed amount of lines.
+    /// The number of columns will then depend on the amount of [Sprite][crate::Sprite]s in the sheet.
     FixedLines(usize),
+    /// Requires that the [SpriteSheet][crate::SpriteSheet] attempts to pack all sprites in a sructure close
+    /// to a square, with as little empty space as possible. The [bool] parameter should be `true` if the algorithm
+    /// should favoritise column length, or `false` to favoritise line length - for cases where one side must be
+    /// larger than the other.
     Packed(bool),
 }
 
 impl Distribution {
+    /// Returns the minimum possible size a [SpriteSheet][crate::SpriteSheet] should have, whilst respecting
+    /// its `sprite_count` and this [Distribution].
     pub fn get_min_size(&self, sprite_count: usize) -> IVec2 {
         match self {
             Self::FixedColumns(columns) => (
