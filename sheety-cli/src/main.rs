c698325e34ca1fae624c6823faafcc9e89324a12
Toni Z <tonizana.dev@gmail.com>
2023-04-29 18:34:27 +0200

    Add usage and version help message

--- sheety-cli/src/main.rs
+++ sheety-cli/src/main.rs
@@ -1,5 +1,9 @@
 extern crate sheety;
 
+static VERSION_MSG: &str = include_str!("version.txt");
+static USAGE_MSG: &str = include_str!("usage.txt");
+static VER_USAGE_MSG: &str = concat!(include_str!("version.txt"), "\n", include_str!("usage.txt"));
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", VER_USAGE_MSG);
 }
